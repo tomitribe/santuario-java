f4e8d64a265f66f15657508eaeabe1e3b8e28c99
Colm O hEigeartaigh <coheigea@apache.org>
2019-06-25 16:49:48 +0000

    Remove DocumentBuilder handling from main code
    git-svn-id: https://svn.apache.org/repos/asf/santuario/xml-security-java/branches/2.1.x-fixes@1862078 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/xml/security/c14n/Canonicalizer.java
+++ src/main/java/org/apache/xml/security/c14n/Canonicalizer.java
@@ -25,8 +25,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.xml.parsers.DocumentBuilder;
-
 import org.apache.xml.security.c14n.implementations.Canonicalizer11_OmitComments;
 import org.apache.xml.security.c14n.implementations.Canonicalizer11_WithComments;
 import org.apache.xml.security.c14n.implementations.Canonicalizer20010315ExclOmitComments;
@@ -252,11 +250,11 @@ public class Canonicalizer {
     public byte[] canonicalize(byte[] inputBytes)
         throws javax.xml.parsers.ParserConfigurationException,
         java.io.IOException, org.xml.sax.SAXException, CanonicalizationException {
+        Document document = null;
         InputStream bais = new ByteArrayInputStream(inputBytes);
         InputSource in = new InputSource(bais);
 
         // needs to validate for ID attribute normalization
-        DocumentBuilder db = XMLUtils.createDocumentBuilder(true, secureValidation);
 
         /*
          * for some of the test vectors from the specification,
@@ -279,14 +277,9 @@ public class Canonicalizer {
          * though the document type declaration is not retained in the
          * canonical form.
          */
-        db.setErrorHandler(new org.apache.xml.security.utils.IgnoreAllErrorHandler());
+        document = XMLUtils.read(in, true, secureValidation,
+             new org.apache.xml.security.utils.IgnoreAllErrorHandler());
 
-        Document document;
-        try {
-            document = db.parse(in);
-        } finally {
-            XMLUtils.repoolDocumentBuilder(db);
-        }
         return this.canonicalizeSubtree(document);
     }
 
